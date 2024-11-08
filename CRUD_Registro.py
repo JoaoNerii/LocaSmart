9ce4aba93049df07785f52ab0a418d28a88b00c8
JoaoNerii <joaogabriel.neri@hotmail.com>
2024-11-07 22:42:15 -0300

    Correção de Bugs

--- CRUD_Registro.py
+++ CRUD_Registro.py
@@ -107,74 +107,78 @@ def login_usuario(cpf, senha):
     print("CPF Ou Senha Incorretos!")
     return False
 
+while True:
+    print("=" * 11, cor.CIANO + "LocaSmart" + cor.RESET, "=" * 12)
+    print(f"|  [{cor.CIANO}1{cor.RESET}] - Menu ADM                |\n|  [{cor.CIANO}2{cor.RESET}] - Login/Cadastro          |\n|  [{cor.CIANO}0{cor.RESET}] - Sair                    |")
+    print("=" * 34)
 
-print("=" * 11, cor.CIANO + "LocaSmart" + cor.RESET, "=" * 12)
-print(f"|  [{cor.CIANO}1{cor.RESET}] - Menu ADM                |\n|  [{cor.CIANO}2{cor.RESET}] - Login/Cadastro          |\n|  [{cor.CIANO}0{cor.RESET}] - Sair                    |")
-print("=" * 34)
+    opcao_login = int(input(cor.CIANO + "Escolha uma opção: " + cor.RESET))
 
-opcao_login = int(input(cor.CIANO + "Escolha uma opção: " + cor.RESET))
+    if opcao_login == 1:
+        login_adm = input("Digite seu Login: ")
+        senha_adm = input("Digite sua Senha: ")
+        if login_adm == "adm" and senha_adm == "adm123":
+            while True:
+                print("=" * 7, cor.CIANO + "Menu ADM | LocaSmart" + cor.RESET, "=" * 5)
+                print(f"|  [{cor.CIANO}1{cor.RESET}] - Listar Usuários         |\n|  [{cor.CIANO}2{cor.RESET}] - Atualizar Usuário       |\n|  [{cor.CIANO}3{cor.RESET}] - Excluir Usuário         |\n|  [{cor.CIANO}0{cor.RESET}] - Voltar                  |")
+                print("=" * 34)
+                opcao = int(input(cor.CIANO + "Escolha uma opção: " + cor.RESET))
+                match (opcao):
+                    case 1:
+                        print("=" * 20)
+                        print("Listando Usuários...")
+                        print("=" * 20)
+                        listar_usuario()
+                    case 2:
+                        atualizar_usuario()
+                    case 3:
+                        excluir_usuario()
+                    case 0:
+                        print("=" * 9)
+                        print("Saindo...")
+                        print("=" * 9)
+                        break
+        else:
+            print("Login ou Senha Inválidos...")
 
-if opcao_login == 1:
-    login_adm = input("Digite seu Login: ")
-    senha_adm = input("Digite sua Senha: ")
-    if login_adm == "adm" and senha_adm == "adm123":
+    elif opcao_login == 2:
+        print("Sessão Login")
         while True:
-            print("=" * 7, cor.CIANO + "Menu ADM | LocaSmart" + cor.RESET, "=" * 5)
-            print(f"|  [{cor.CIANO}1{cor.RESET}] - Listar Usuários         |\n|  [{cor.CIANO}2{cor.RESET}] - Atualizar Usuário       |\n|  [{cor.CIANO}3{cor.RESET}] - Excluir Usuário         |\n|  [{cor.CIANO}0{cor.RESET}] - Sair                    |")
+            print("=" * 11, cor.CIANO + "LocaSmart" + cor.RESET, "=" * 12)
+            print(f"|  [{cor.CIANO}1{cor.RESET}] - Cadastrar Novo Usuário  |\n|  [{cor.CIANO}2{cor.RESET}] - Login                   |\n|  [{cor.CIANO}0{cor.RESET}] - Voltar                  |")
             print("=" * 34)
             opcao = int(input(cor.CIANO + "Escolha uma opção: " + cor.RESET))
             match (opcao):
                 case 1:
-                    print("=" * 20)
-                    print("Listando Usuários...")
-                    print("=" * 20)
-                    listar_usuario()
+                    print("-" * 27)
+                    print(f"{cor.CIANO}Cadastrando Novo Usuário...{cor.RESET}")
+                    print("-" * 27)
+                    nome = input("Digite seu nome: ")
+                    cpf = input("Digite seu CPF: ")
+                    telefone = input("Digite seu telefone: ")
+                    endereco = input("Digite seu endereço: ")
+                    cep = input("Digite seu CEP: ")
+                    email = input("Digite seu email: ")
+                    senha = input("Digite sua senha: ")
+                    idade = input("Digite sua idade: ")
+                    adicionar_usuario(nome, cpf, telefone, endereco, cep, email, senha, idade)
                 case 2:
-                    atualizar_usuario()
-                case 3:
-                    excluir_usuario()
+                    cpf = input("Digite seu CPF: ")
+                    senha = input("Digite sua Senha: ")
+                    login_usuario(cpf, senha)
                 case 0:
-                    print("=" * 9)
-                    print("Saindo...")
-                    print("=" * 9)
-                    continue
+                    print("=" * 13)
+                    print(f"| {cor.CIANO}Saindo...{cor.RESET} |")
+                    print("=" * 13)
+                    break
+        
+
+    elif opcao_login == 0:
+        print("=" * 13)
+        print(f"| {cor.CIANO}Saindo...{cor.RESET} |")
+        print("=" * 13)
+        break
+
     else:
-        print("Login ou Senha Inválidos...")
-
-elif opcao_login == 2:
-    print("Sessão Login")
-    while True:
-        print("=" * 11, cor.CIANO + "LocaSmart" + cor.RESET, "=" * 12)
-        print(f"|  [{cor.CIANO}1{cor.RESET}] - Cadastrar Novo Usuário  |\n|  [{cor.CIANO}2{cor.RESET}] - Login                   |\n|  [{cor.CIANO}0{cor.RESET}] - Sair                    |")
-        print("=" * 34)
-        opcao = int(input(cor.CIANO + "Escolha uma opção: " + cor.RESET))
-        match (opcao):
-            case 1:
-                print("-" * 27)
-                print("Cadastrando Novo Usuário...")
-                print("-" * 27)
-                nome = input("Digite seu nome: ")
-                cpf = input("Digite seu CPF: ")
-                telefone = input("Digite seu telefone: ")
-                endereco = input("Digite seu endereço: ")
-                cep = input("Digite seu CEP: ")
-                email = input("Digite seu email: ")
-                senha = input("Digite sua senha: ")
-                idade = input("Digite sua idade: ")
-                adicionar_usuario(nome, cpf, telefone, endereco, cep, email, senha, idade)
-            case 2:
-                cpf = input("Digite seu CPF: ")
-                senha = input("Digite sua Senha: ")
-                login_usuario(cpf, senha)
-            case 0:
-                print("=" * 9)
-                print("Saindo...")
-                print("=" * 9)
-                break
-    
-
-elif opcao_login == 0:
-    print("Saindo...")
-
-else:
-    print(f"{cor.VERMELHO}Opção Inválida!{cor.RESET}")
+        print(f"{cor.VERMELHO}Opção Inválida!{cor.RESET}")
+        
